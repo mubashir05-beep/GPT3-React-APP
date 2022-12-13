718314aeeac3c6471c092083c9588a1582d8e9b0
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-12-12 17:33:30 -0800

    added some more components

--- gpt3-app/src/components/cta/Cta.jsx
+++ gpt3-app/src/components/cta/Cta.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './cta.css'
-const Cta = () => {
+const CTA = () => {
   return (
-    <div>Cta</div>
+    <div>CTA</div>
   )
 }
 
-export default Cta
+export default CTA
---
+++ gpt3-app/src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGPT3.css'
+const WhatGPT3 = () => 
+   (
+    <div>WhaGPT3</div>
+  )
+
+
+export default WhatGPT3
