6eac4c58cf63428dcc315c47e23e9717322c3749
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-12-15 20:22:52 -0800

    Completed the nav bar and header section now working on the brands section

--- gpt3-app/src/components/navbar/Navbar.jsx
+++ gpt3-app/src/components/navbar/Navbar.jsx
@@ -2,25 +2,6 @@ import React, {useState} from 'react';
 import './navbar.css';
 import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
-const Menu = () => {
-  <>
-    <p>
-      <a href="#home">Home</a>
-    </p>
-    <p>
-      <a href="#wgpt3">What is GPT3?</a>
-    </p>
-    <p>
-      <a href="#possibility">Open AI</a>
-    </p>
-    <p>
-      <a href="#features">Case Studies</a>
-    </p>
-    <p>
-      <a href="#blog">Library</a>
-    </p>
-  </>
-}
 //BEM -> Block Element Modifier
 const Navbar = () => {
   const [toggleMenu, setToggleMenu]=useState(false);
