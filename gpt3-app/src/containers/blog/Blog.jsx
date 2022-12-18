fb91838c3570760d5e6c7674888147261ce5229f
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-12-17 22:09:46 -0800

    Update Blog.jsx

--- gpt3-app/src/containers/blog/Blog.jsx
+++ gpt3-app/src/containers/blog/Blog.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './blog.css'
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-headTitle">
+        A lot is happening, We are blogging about it.
+      </div>
+    </div>
+  );
 }
 
 export default Blog
