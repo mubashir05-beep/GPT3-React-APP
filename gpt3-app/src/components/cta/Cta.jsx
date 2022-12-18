d18b85d083958eef215156cc5807b5a787f478ff
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-12-17 22:09:42 -0800

    Made the features section more responsive and fixed some errors and added a new CTA section now started working on BLOG section.

--- gpt3-app/src/components/cta/Cta.jsx
+++ gpt3-app/src/components/cta/Cta.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import './cta.css'
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
+    <div className="gpt3__cta-container section__padding">
+      <div className="gpt3__cta-container__text">
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div className="gpt3__container__ctaBTN">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 }
 
 export default CTA
--- gpt3-app/src/containers/possibility/Possibility.jsx
+++ gpt3-app/src/containers/possibility/Possibility.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import './possibility.css'
+import './possibility.css';
+import Image from "../../assets/possibility.png";
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
+    <div className="gpt3__possiblities" id="possibility">
+      <div className="gpt3__possiblities-headImg">
+        <img src={Image} alt="Product Showcase Image" />
+      </div>
+
+      <div className="gpt3__possibilities-text_Data">
+        <p className="blueLink">Request Early Access to Get Started</p>
+        <h1 className="gradient__text font">
+          The possibilities are beyond your imagination
+        </h1>
+        <p className="subText">
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <p className="getStartedLink">Request Early Access to Get Started</p>
+      </div>
+    </div>
+  );
 }
 
 export default Possibility
